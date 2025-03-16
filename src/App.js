cc7f13e09f86f79542f2673d0be1bd9e0c135438
Diyana-nikk <xdiyana.nx@gmail.com>
2025-03-16 17:53:38 +0000

    set up react website

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to My Personal Website</h1>
+      <p>Hello! I'm [Your Name], and this is my website.</p>
+    </div>
+  );
+}
+
+export default App;
