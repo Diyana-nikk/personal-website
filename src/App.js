f7960eb114e803b2af94634470cea4ce78537439
Diyana-nikk <xdiyana.nx@gmail.com>
2025-03-16 17:57:56 +0000

    removing logo

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>Welcome to My Personal Website</h1>
-      <p>Hello! I'm [Your Name], and this is my website.</p>
+      <p>Hello! I'm Diyana, and this is my website.</p>
     </div>
   );
 }
